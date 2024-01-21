ca02d16952adef497893da0440d706d6519f7492
prplecloud <52612580+prplecloud@users.noreply.github.com>
2024-01-22 00:32:32 +0100

    oe flemme

--- templates/templates.go
+++ templates/templates.go
@@ -8,13 +8,12 @@ import (
 var Temp *template.Template
 
 func InitTemplate() {
-	
+
 	temp, errTemp := template.ParseGlob("./templates/*.html")
 	if errTemp != nil {
 		fmt.Printf("Erreur template: %v", errTemp.Error())
 		return
 	}
 	Temp = temp
-
 
 }
